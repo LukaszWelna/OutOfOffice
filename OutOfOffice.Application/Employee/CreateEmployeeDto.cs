4ba8e3a7480342314633011253f113366a28b86a
Lukasz Welna <lukasz.welna96@gmail.com>
2024-06-19 22:31:31 +0200

    Added form to create new employee

---
+++ OutOfOffice.Application/Employee/CreateEmployeeDto.cs
@@ -0,0 +1,24 @@
+﻿using OutOfOffice.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOffice.Application.Employee
+{
+    public class CreateEmployeeDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = default!;
+        public int SubdivisionId { get; set; }
+        public List<Subdivision> Subdivisions { get; set; } = new List<Subdivision>();
+        public int PositionId { get; set; }
+        public List<Position> Positions { get; set; } = new List<Position>();
+        public bool Status { get; set; }
+        public int PeoplePartnerId { get; set; }
+        public List<OutOfOffice.Domain.Entities.Employee> PeoplePartners { get; set; } = new List<OutOfOffice.Domain.Entities.Employee>();
+        public int OutOfOfficeBalance { get; set; }
+        public byte[]? Photo { get; set; }
+    }
+}
---
+++ OutOfOffice.Application/Employee/GetEmployeeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOffice.Application.Employee
+{
+    public class GetEmployeeDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = default!;
+        public string Subdivision { get; set; } = default!;
+        public string Position { get; set; } = default!;
+        public bool Status { get; set; }
+        public int PeoplePartnerId { get; set; }
+        public int OutOfOfficeBalance { get; set; }
+    }
+}
